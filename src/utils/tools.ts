26cbc159c1b3a9c757a47e42082664758abd68bf
cccxk11123 <86996705+cccxk11123@users.noreply.github.com>
2023-02-22 14:19:51 +0800

    Delete src directory

--- src/utils/tools.ts
+++
@@ -1,8 +0,0 @@
-export const serverUrl = "http://qiaohuhu.xyz:9200/api";
-
-export const setToken = (token: string) =>
-  sessionStorage.setItem("token", token);
-
-export const clearToken = () => sessionStorage.clear();
-
-export const getToken = () => sessionStorage.getItem("token");
